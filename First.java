be4f36a5190793e66a1a0b965d3ad67906f1cda1
Azer <105559540+AzerLazer@users.noreply.github.com>
2022-05-19 18:25:49 +0300

    Add files via upload

---
+++ First.java
@@ -0,0 +1,18 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Введите число: ");
+            int a = sc.nextInt();
+            if (a > 7) {
+                System.out.println("Привет");
+            }
+        } catch (Exception e) {
+            System.err.println("Введено не число");
+        }
+    }
+}
